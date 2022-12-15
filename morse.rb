78cc7fefde25c0f0c664b84d86e9d0ed874a743d
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-12-15 11:20:41 +0000

    Add decode morse method

--- morse.rb
+++ morse.rb
@@ -83,4 +83,75 @@ def decode_word(morse_code)
   decode_word("-- -.--") # Returns "MY"
   decode_word(".- -... -.-.") # Returns "ABC"
 
+  # Define a hash that maps each Morse code character to its corresponding
+# letter or number
+MORSE_CODE = {
+  ".-" => "A",
+  "-..." => "B",
+  "-.-." => "C",
+  "-.." => "D",
+  "." => "E",
+  "..-." => "F",
+  "--." => "G",
+  "...." => "H",
+  ".." => "I",
+  ".---" => "J",
+  "-.-" => "K",
+  ".-.." => "L",
+  "--" => "M",
+  "-." => "N",
+  "---" => "O",
+  ".--." => "P",
+  "--.-" => "Q",
+  ".-." => "R",
+  "..." => "S",
+  "-" => "T",
+  "..-" => "U",
+  "...-" => "V",
+  ".--" => "W",
+  "-..-" => "X",
+  "-.--" => "Y",
+  "--.." => "Z",
+  "-----" => "0",
+  ".----" => "1",
+  "..---" => "2",
+  "...--" => "3",
+  "....-" => "4",
+  "....." => "5",
+  "-...." => "6",
+  "--..." => "7",
+  "---.." => "8",
+  "----." => "9"
+}
+
+# Define a method that takes a string of Morse code as input and returns
+# the corresponding string of letters and numbers
+def decode_morse(code)
+  # Split the input string into an array of individual Morse code words,
+  # separated by 3 spaces
+  words = code.split("   ")
+
+  # Map each Morse code word to its corresponding string of letters and
+  # numbers, and join the resulting array into a single string, separated
+  # by a single space
+  words.map do |word|
+    # Split the Morse code word into an array of individual Morse code
+    # characters, separated by a single space
+    chars = word.split(" ")
+
+    # Map each Morse code character to its corresponding letter or number
+    # and join the resulting array into a single string
+    chars.map { |char| MORSE_CODE[char] }.join
+  end.join(" ")
+end
+
+# Test the method with a few examples
+puts decode_morse(".-   -...   -.-.   -..   .")
+# Output: "A B C D E"
+
+puts decode_morse(".---   ...--   -....   ---..   ----.")
+# Output: "J 3 6 8 9"
+
+puts decode_morse(".... . .-.. .-.. ---   .-- --- .-. .-.. -..")
+
   
